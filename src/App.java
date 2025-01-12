e57094e41ffae55ae9a1417cbfd144940d0fdf9e
Sebas <aviladiego163@gmail.com>
2025-01-11 22:34:17 -0500

    Ejercicio 3

--- src/App.java
+++ src/App.java
@@ -1,11 +1,40 @@
 
+import java.util.List;
+
+import main.Ejercicio_03_listLeves.ListLeves;
 import main.Materia.Controllers.ArbolBinario;
 import main.Materia.Controllers.ArbolRecorridos;
+import main.Materia.Controllers.Models.Node;
 
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
-        runArbolBinario();
+        //runArbolBinario();
+
+        Node root = new Node(7);
+        root.setLeft(new Node(5));
+        root.setRight(new Node(4));
+        root.getLeft().setLeft(new Node(8));
+        root.getLeft().setRight(new Node(3));
+        
+        ListLeves list = new ListLeves();
+        
+        
+        List<List<Node>> niveles = list.listLeves(root);
+        
+        
+        if (niveles != null) {
+           
+            for (List<Node> level : niveles) {
+                for (Node node : level) {
+                    System.out.print(node.getValue() + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("El árbol está vacío.");
+        }
+
        
 
     }
--- src/main/Ejercicio_03_listLeves/ListLeves.java
+++ src/main/Ejercicio_03_listLeves/ListLeves.java
@@ -1,6 +1,9 @@
 package main.Ejercicio_03_listLeves;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 //import org.w3c.dom.Node;
 
@@ -8,8 +11,38 @@ import main.Materia.Controllers.Models.Node;
 
 public class ListLeves {
     public List<List<Node>> listLeves(Node root){
-            return null;
 
+        List<List<Node>> result = new ArrayList<>(); 
+    if (root == null) {
+        return result; 
     }
+    
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root); 
+    
+    while (!queue.isEmpty()) {
+        int levelSize = queue.size(); 
+        List<Node> currentLevel = new ArrayList<>(); 
+        
+        
+        for (int i = 0; i < levelSize; i++) {
+            Node currentNode = queue.poll(); 
+            currentLevel.add(currentNode); 
+            
+            
+            if (currentNode.getLeft() != null) {
+                queue.add(currentNode.getLeft());
+            }
+            if (currentNode.getRight() != null) {
+                queue.add(currentNode.getRight());
+            }
+        }
+        
+        
+        result.add(currentLevel);
+    }
+    
+    return result;
+}
     
 }
